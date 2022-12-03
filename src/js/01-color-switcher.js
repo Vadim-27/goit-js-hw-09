3c57a9f9cb37cf6692537dacdbf46c70964de385
Vadim-27 <vadim.bulanyi@gmail.com>
2022-12-03 11:43:35 +0200

    1 з

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+import '../css/common.css';
 
+const refs = {
+    startBtnEl: document.querySelector('button[data-start]'),
+    stopBtnEl: document.querySelector('button[data-stop]'),
+};
+
+const CHANGEBACKGRCOLOR_DELEY = 1000;
+let timeId = null;
+
+
+refs.startBtnEl.addEventListener('click', startCengeColor);
+refs.stopBtnEl.addEventListener('click', stopCengeColor);
+
+function startCengeColor() {
+    timeId = setTimeout(() => {
+        document.body.style.background.querySelector = getRandomHexColor();
+    }, CHANGEBACKGRCOLOR_DELEY);
+    refs.startBtnEl.setAttribute('disabled', true);
+    refs.stopBtnEl.removeAttribute('disabled');
+};
+
+function stopCengeColor() {
+    clearInterval(timeId);
+    refs.stopBtnEl.setAttribute('disabled', true);
+    refs.startBtnEl.removeAttribute('disabled');
+}
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
